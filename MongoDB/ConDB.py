423ef6c2a866da92168a665c59a6a27ac0e588f9
Elina Zarisheva <elina.zarisheva@gmail.com>
2015-01-24 16:14:54 +0100

    connection to MongoDb, import in mongo

--- MongoDB/ConDB.py
+++ MongoDB/ConDB.py
@@ -2,30 +2,24 @@ import pymongo
 from pymongo import Connection
 
 ##########################
-####Connect to MongoDB ###
+# Connect to MongoDB
 ##########################
 
-
-#######
-#mongodb creates databases and collections automatically for you if they dont exist already.
-#The DB will be schown firstly if there is smth already
-#####
+# mongodb creates databases and collections automatically for you if they dont exist already.
+# The DB will be schown firstly if there is smth already
 
 
-#default connection
-connection = Connection()
+def mongoDbConnection(collectionName):
+    # default connection
+    connection = Connection()
+    # Getting/Creating  DB
+    db = connection['DARecognition']
+    # Getting/Creating Collection
+    collection = db[collectionName]
+    return collection
 
+# ####Insert data into collection ####
+# collection.insert({"twitterId" : "11111", "text": "blablabla"})
+# collection.insert({"twitterId" : "122222", "text": "bleeeebla"})
 
-####Getting/Creating  DB #### 
-
-db = connection['test-database1']
-
-####Getting/Creating Collection ####
-
-collection = db['test-collection1']
-
-####Insert data into collection ####
-collection.insert({"twitterId" : "11111", "text": "blablabla"})
-collection.insert({"twitterId" : "122222", "text": "bleeeebla"})
-
-
+# #### Delete unnesessary DB ####
---
+++ MongoDB/importTwitterConversation.py
@@ -0,0 +1,31 @@
+import conDB
+import re
+import json
+
+
+def importData(collectionName, data):
+    collection = conDB.mongoDbConnection(collectionName)
+    for tweet in iterparse(data):
+        collection.insert(tweet)
+
+nonspace = re.compile(r'\S')
+
+
+def iterparse(j):
+    decoder = json.JSONDecoder()
+    pos = 0
+    while True:
+        matched = nonspace.search(j, pos)
+        if not matched:
+            break
+        pos = matched.start()
+        decoded, pos = decoder.raw_decode(j, pos)
+        yield decoded
+
+# with open("test.txt") as f:
+#     content = f.read()
+# for decoded in iterparse(content):
+#     print(decoded)
+
+
+#importData("testDataTweets", content)
