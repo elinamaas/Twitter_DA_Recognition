f50d9be3a5794adcfd40315abe3beb279f36501f
Elina Zarisheva <elina.zarisheva@gmail.com>
2015-01-26 11:44:36 +0100

    import data, check db, count posts

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-
+from readData import readFileWithData
+from mongoDB import conDB,importTwitterConversation, queries
 import glob
 import os
 
-for filename in glob.iglob(os.path.join('../../DATA', '*', '*.txt')):
-    content = readTXT(filename)
-    importTwitterConversation.importData('test2', content)
+database = 'DARecognition'
+collectionRawTwitterData = 'rawTwitterData'
+
+if (conDB.checkAmount(database, collectionRawTwitterData) == 0):
+### Import raw twitter conversation in DB ##########
+    for filename in glob.iglob(os.path.join('DATA/twitterConversation','*.txt')):
+        content = readFileWithData.readTXT(filename)
+        print filename + ' will be added to DB'
+        importTwitterConversation.importData(database, collectionRawTwitterData, content)
+        print filename + ' is added to DB'
+else:
+    print 'collection ' + collectionRawTwitterData + ' is already exists'
+
+amountOfTweets = conDB.checkAmount(database, collectionRawTwitterData)
+print 'There are ',  amountOfTweets, ' tweets in DB.'
+queries.searchFirstTweet(database, collectionRawTwitterData )
