21a260d64a1a2ca791e29def984d4936c2dfd2b3
Elina Zarisheva <elina.zarisheva@gmail.com>
2015-02-03 14:41:31 +0100

    import annotated docs

--- readData/annotatedData_read.py
+++ readData/annotatedData_read.py
@@ -8,13 +8,9 @@ from json import JSONEncoder
 from mongoDB import importAnnotatedData
 
 
-def read_annotated_docs(directory_path, collection_raw_twitter_data, collection_annotated_data):
+def read_annotated_docs(directory_path, collection_annotated_data):
     conversation_id = 0
-    tweet_id = 0
-    token = ''
-    tag = ''
     data = {}
-    json_data = None
     id = 1
     for directory_name in glob.iglob(os.path.join(directory_path,'*.*')):
         for filename in glob.iglob(os.path.join(directory_name, '*.tsv')):
@@ -25,42 +21,45 @@ def read_annotated_docs(directory_path, collection_raw_twitter_data, collection_
                 for row in content:
                     if len(row) is 0:
                         if conversation_id is not '1':
-                            # in_replay_to_status_id = find_in_replay_to_the_status(collection_raw_twitter_data,
-                            #                                                       data['tweet_id'])
-                            # data['in_replay_to_status_id'] = in_replay_to_status_id
                             data['id'] = id
                             id += 1
                             importAnnotatedData.importAnnotatedData(data, collection_annotated_data)
                         else:
                             continue
                     else:
-                        if check_if_new_thread(row) == True:
+                        if check_if_new_thread(row) is True:
                             conversation_id = '1'
                         if '#id' in row[0]:
                             conversation_id = re.split('id=', row[0])[1]
                             data = {}
                             data['conversation_id'] = conversation_id
-                        if len(previous_row) is not 0 and '#id' in previous_row[0] and conversation_id is not '1':
+                        elif check_if_consistent(row) is False:
+                            conversation_id = '1'
+                        elif conversation_id == '1':
+                            continue
+                        elif len(previous_row) is not 0 and '#id' in previous_row[0]:
                             tweet_id = re.split('id=', row[0])[1]
                             tweet_id = re.split(' user', tweet_id)[0]
                             data['tweet_id'] = tweet_id
                             data['text'] = row[0]
-                        if conversation_id == re.split('-', row[0])[0] and conversation_id is not '1' \
-                                and re.split('-', row[0])[1] not in ['1', '2', '3']:
-                            #######SPLIT NUMBERS!
+                        elif conversation_id == re.split('-', row[0])[0]  \
+                                and re.split('-', row[0])[1] not in ['1', '2', '3'] and len(row) > 3:
                             if 'user=' in row[1] or '@' in row[1]:
-                                # token = row[1].replace('.', '_')
-                                token = convertCharacters.replace_german_letters(row[1])
+                                token = row[1]
                                 tag = '0'
-                                data[re.split('-', row[0])[1]] = [token,tag]
+                                data[re.split('-', row[0])[1]] = [token, tag]
                             else:
-                                token = row[1].replace('.', '_')
-                                token = convertCharacters.replace_german_letters(row[1])
+                                token = row[1]
+                                # token = row[1].replace('.', '_')
+                                # token = convertCharacters.replace_german_letters(row[1])
                                 if row[2] == 'O':
                                     tag = '0'
                                 else:
                                     tag = row[2]
                                 data[re.split('-', row[0])[1]] = [token, tag]
+                        elif len(row) < 3:
+                            conversation_id = 1
+
                     previous_row = row
                 #print content
 
@@ -82,4 +81,13 @@ def check_if_new_thread(row):
         return True
     else:
         return False
-#def find_tweet_id(row):
+
+
+def check_if_consistent(row):
+    if 'text=' in row[0]:
+        if 'user=' in row[0] and 'id=' in row[0]:
+            return True
+        else:
+            return False
+    else:
+        return True
