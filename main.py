d260a58777c115f439e7e96af3a67fa23619e191
Elina Zarisheva <elina.zarisheva@gmail.com>
2015-02-16 17:12:01 +0100

    editing annotated tweets

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 __author__ = 'snownettle'
 from readData import rawTwitterData, annotatedData_read
 from mongoDB import conDB, importTwitterConversation, queries
+from AnnotatedData.editing_annotated_tweets import segmentation, merge_annotations
+from AnnotatedData.tweets_for_editing import write_to_file
 from statistics import annotatedData_stat
 import glob
 import os
@@ -8,7 +10,7 @@ import tweet
 
 
 database = 'DARecognition'
-# collectionRawTwitterData = 'rawTwitterData'
+
 # make all connection to db here
 collectionRawTwitterData = conDB.get_collection(database, 'rawTwitterData')
 collectionAnnotatedData = conDB.get_collection(database, 'annotatedTwitterData')
@@ -18,31 +20,26 @@ annotatedData_path = 'DATA/annotationed/webanno-projectexport/annotation'
 if conDB.check_tweets_amount(collectionRawTwitterData) == 0:
     #  Import raw twitter conversation in DB
     rawTwitterData.import_raw_twitter_data(rawTwitterConversation_path, collectionRawTwitterData)
-    # for filename in glob.iglob(os.path.join('DATA/test','*.txt')):
-    #     content = rawTwitterData.readTXT(filename)
-    #     print filename + ' will be added to DB'
-    #     importTwitterConversation.importData(collectionRawTwitterData, content)
-    #     print filename + ' is added to DB'
+
 else:
     print 'Collection ' + collectionRawTwitterData.name + ' is already existed'
 
 amountOfRawTweets = conDB.check_tweets_amount(collectionRawTwitterData)
 print 'There are ',  amountOfRawTweets, ' raw tweets in DB.'
-# queries.search_first_tweet(database, collectionRawTwitterData)
-#conversations = queries.check_lang(database, collectionRawTwitterData)
-# conversations = queries.build_conversation(collectionRawTwitterData)
-# for conversation in conversations:
-#     conversation.conversation_length()
 
-
-# collectionAnnotatedData.drop()
 
 if conDB.check_tweets_amount(collectionAnnotatedData) == 0:
     annotatedData_read.read_annotated_docs(annotatedData_path, collectionAnnotatedData)
 else:
     print 'Collection ' + collectionAnnotatedData.name + ' is already existed'
 amountOdAnnotatedTweets = conDB.check_tweets_amount(collectionAnnotatedData)
-# print 'There are ', amountOdAnnotatedTweets, ' annotated tweets by linguistic students in DB'
 
-annotatedData_stat.segmentation(collectionAnnotatedData)
+list_of_annotated_tweets = segmentation(collectionAnnotatedData)
+annotatedData_stat.number_of_annotated_tweet(list_of_annotated_tweets)
+annotatedData_stat.numbers_of_tweets_agreed_by_three(list_of_annotated_tweets)
+
+list_of_annotated_tweets = merge_annotations(list_of_annotated_tweets)
+list_of_tweets_for_editing = annotatedData_stat.numbers_of_agreed_tweets_after_merging(list_of_annotated_tweets)
+
+write_to_file(list_of_tweets_for_editing)
 
