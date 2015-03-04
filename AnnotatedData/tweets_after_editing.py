c5d0b58d58823fb0c66abc1e19d87e3947aff069
Elina Zarisheva <elina.zarisheva@gmail.com>
2015-03-04 14:05:52 +0100

    write in file annotated tweets after editing

--- AnnotatedData/tweets_after_editing.py
+++ AnnotatedData/tweets_after_editing.py
@@ -2,8 +2,8 @@ __author__ = 'snownettle'
 import csv
 
 
-def write_to_file(list_of_tweets):
-    with open('tweet_to_edit.csv', 'wb') as csvfile:
+def write_to_file(list_of_tweets, file_name):
+    with open(file_name, 'wb') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=';',)
         spamwriter.writerow(['offset', 'token', 'variant 1', 'variant 2', 'variant 3', 'variant 4', 'dialogue act'])
         for tweet in list_of_tweets:
@@ -13,20 +13,21 @@ def write_to_file(list_of_tweets):
             text = text.encode('utf-8')
 
             spamwriter.writerow([text])
-            tokens = tweet.get_token()
+            tokens = tweet.get_tags()
             for offset, tags_list in tokens.iteritems():
                 word = tweet.get_word(offset)
                 word = word.encode('utf-8')
                 if len(tags_list) == 1:
-                    if 3 in tags_list[0].values():
-                        for tag_name, value in tags_list[0].iteritems():
-                            spamwriter.writerow([offset, word, '-', '-', '-', '-', tag_name])
+                    # if 3 in tags_list[0].values():
+                    for tag_name, value in tags_list.iteritems():
+                        # if value == 3:
+                        spamwriter.writerow([offset, word, '-', '-', '-', '-', tag_name])
                 else:
                     data = list()
                     data.append(offset)
                     data.append(word)
-                    for tag in tags_list:
-                        for tag_name, value in tag.iteritems():
-                            data.append(tag_name)
+                    # for tag in tags_list:
+                    for tag_name, value in tags_list.iteritems():
+                        data.append(tag_name)
                     spamwriter.writerow(data)
 
