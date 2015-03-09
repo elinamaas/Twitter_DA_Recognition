33eb54aba27a01ab7a2d3975bb1ff564ca34ccfd
Elina Zarisheva <elina.zarisheva@gmail.com>
2015-03-09 10:47:10 +0100

    statistics for annotated data

--- statistics/annotatedData_stat.py
+++ statistics/annotatedData_stat.py
@@ -1,5 +1,7 @@
 __author__ = 'snownettle'
 
+from mongoDB import queries
+
 
 def number_of_annotated_tweet(list_of_tweets):
     print 'There are ', len(list_of_tweets), ' annotated tweets by linguistic students in DB'
@@ -33,15 +35,13 @@ def numbers_of_agreed_tweets_after_merging(list_of_tweets):
     list_of_tweets_for_editing = list()
     list_of_tweets_done = list()
     for tweet in list_of_tweets:
-        same_dicision = 0
+        same_decision = 0
         token_dictionary = tweet.get_tags()
         token_number = len(token_dictionary)
         for offset, tags in token_dictionary.iteritems():
             if len(tags) == 1:
-                for tag_name, agreed in tags.iteritems():
-                    # if agreed == 3:
-                    same_dicision += 1
-        if same_dicision == token_number:
+                same_decision += 1
+        if same_decision == token_number:
             argeed_with_tags += 1
             list_of_tweets_done.append(tweet)
 
@@ -50,5 +50,28 @@ def numbers_of_agreed_tweets_after_merging(list_of_tweets):
     print 'After merging there are ' + str(len(list_of_tweets_done)) + ' done tweets'
     print 'There are ' + str(len(list_of_tweets_for_editing)) + ' to edit'
     return list_of_tweets_done, list_of_tweets_for_editing
+
+
+# def count_conversation(collection):
+#     queries.build_conversation(collection)
+
+
+def segments_in_tweet(list_of_annotated_tweets):
+    number_of_segments = dict()
+    average_number_of_tweets = list()
+    overal_segments = int()
+    for tweet in list_of_annotated_tweets:
+        segments = tweet.get_segmentation()
+        occurancy = len(segments)
+        if occurancy in number_of_segments:
+            number_of_segments[occurancy] += 1
+        else:
+            number_of_segments[occurancy] = 1
+    for number, value in number_of_segments.iteritems():
+        print 'Number of segments: ', number, ' occur: ', value, ' times'
+    #     average_number_of_tweets.append(number)
+    #     overal_segments += value
+    # print 'Average number of segments:', (sum(average_number_of_tweets)/float(overal_segments))
+
 
 
