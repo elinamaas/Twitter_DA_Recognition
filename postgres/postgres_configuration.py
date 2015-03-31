4bd89c7783af3c8c4b4ff0435ebcca5c96666b00
Elina Zarisheva <elina.zarisheva@gmail.com>
2015-03-31 08:14:38 +0200

    connection to postgres, define DB

--- postgres/postgres_configuration.py
+++ postgres/postgres_configuration.py
@@ -1,17 +1,41 @@
 __author__ = 'snownettle'
 
 import psycopg2
+import sys
 
-# Try to connect
 
-try:
-    conn = psycopg2.connect("dbname='dialogue_acts' user='postgres' password='postgres'")
-    print "DB exists"
-except:
-    print "I am unable to connect to the database."
+def make_connection():
+    con = None
 
-    # cur = conn.cursor()
-    # try:
-    # cur.execute("""DROP DATABASE foo_test""")
-    # except:
-    #     print "I can't drop our test database!"
+    try:
+
+        con = psycopg2.connect("dbname='dialogue_acts' user='postgres'")
+
+        cur = con.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS Tweet(Tweet_id BIGINT PRIMARY KEY, "
+                    "In_replay_to BIGINT, Text VARCHAR(255))")
+        cur.execute("CREATE TABLE IF NOT EXISTS Annotated_tweets(Tweet_id BIGINT PRIMARY KEY, "
+                    "In_replay_to BIGINT, FOREIGN KEY (Tweet_id) REFERENCES Tweet (Tweet_id))")
+        cur.execute('CREATE TABLE IF NOT EXISTS Dialogue_act (Dialogue_act_id INTEGER PRIMARY KEY, '
+                    'Dialogue_act_name VARCHAR(100))')
+        cur.execute('CREATE TABLE IF NOT EXISTS Token_tweet (Tweet_id BIGINT, '
+                    'Token_offset INTEGER, Token VARCHAR(50), Dialogue_act_id INTEGER, '
+                    'PRIMARY KEY(Tweet_id, Token_offset), '
+                    'FOREIGN KEY(Tweet_id) REFERENCES Tweet (Tweet_id),'
+                    'FOREIGN KEY(Dialogue_act_id) REFERENCES Dialogue_act (Dialogue_act_id))')
+        cur.execute('CREATE TABLE IF NOT EXISTS Segmentation(Tweet_id BIGINT PRIMARY KEY, '
+                    'Segmentation_offsets VARCHAR(10),'
+                    ' Dialogue_act_id INTEGER, FOREIGN KEY(Tweet_id) REFERENCES Tweet (Tweet_id), '
+                    'FOREIGN KEY(Dialogue_act_id) REFERENCES  Dialogue_act(Dialogue_act_id))')
+
+        con.commit()
+    except psycopg2.DatabaseError, e:
+        if con:
+            con.rollback()
+        print 'Error %s' % e
+        sys.exit(1)
+    return con, cur
+
+
+def close_connection(connection):
+    connection.close()
