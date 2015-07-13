ae636b75fdd74f93e5555be73cac31b0295f12fe
Elina Zarisheva <elina.zarisheva@gmail.com>
2015-07-13 17:23:53 +0200

    refractor

--- statistics/rawData_stat.py
+++ statistics/rawData_stat.py
@@ -50,13 +50,14 @@ def longest_conversation(conversation_list):
             longest_depth = discussion.find_depth()
     print 'The longest conversation has depth: ' + str(longest_depth)
 
-# print 'start'
-# collectionRawTwitterData = mongoDB_configuration.get_collection('DARecognition', 'rawTwitterData')
-# indexes = collectionRawTwitterData.index_information()
+print 'start'
+collectionRawTwitterData = mongoDB_configuration.get_collection('DARecognition', 'rawTwitterData')
+indexes = collectionRawTwitterData.index_information()
 # print indexes
-# collectionRawTwitterData.ensure_index('id')
-# collectionRawTwitterData.ensure_index('in_reply_to_status_id')
-# conversation_list = conversation.build_conversation(collectionRawTwitterData)
-# number_of_conversation_with_replays(conversation_list)
-# depth_of_conversation(conversation)
-# longest_conversation(conversation_list)
+collectionRawTwitterData.ensure_index('id')
+collectionRawTwitterData.ensure_index('in_reply_to_status_id')
+conversation_list = conversation.build_conversation(collectionRawTwitterData)
+
+number_of_conversation_with_replays(conversation_list)
+depth_of_conversation(conversation)
+longest_conversation(conversation_list)
--- twitter_objects/conversation.py
+++ twitter_objects/conversation.py
@@ -1,10 +1,11 @@
 __author__ = 'snownettle'
 # from __future__ import division
-from mongoDB import queries
+from mongoDB import mongoDBQueries
 from tweet import Tweet
 from mongoDB import mongoDB_configuration
 from treelib import Tree
 import sys
+from general import check_lang
 import nltk
 
 
@@ -23,7 +24,7 @@ class Conversation:
         self.tweets_id.append(tweet)
 
     def set_depth(self):
-        self.depth = self.conversation_tree.depth()
+        self.depth = self.conversation_tree.depth() + 1
 
     def find_depth(self):
         return self.depth
@@ -32,7 +33,7 @@ class Conversation:
         return self.tweets_id
 
     def set_width(self):
-        self.width = len(self.conversation_tree.all_nodes()) + 1
+        self.width = len(self.tweets_id)
 
     def find_width(self):
         return self.width
@@ -43,10 +44,10 @@ class Conversation:
 
 def build_conversation(collection):
     sys.setrecursionlimit(1500)
-    root_tweets = queries.search_root_tweets(collection)
+    root_tweets = mongoDBQueries.search_root_tweets(collection)
     conversations_list = list()
     tweets_list = set()
-    test_list = list()
+    test_list = set()
     count = 0
     number_of_root_tweets = root_tweets.count()
     for root_tweet in collection.find({'in_reply_to_status_id': None}).batch_size(500):
@@ -57,56 +58,62 @@ def build_conversation(collection):
             continue
 
         else:
-            tweets_list.add(tweet_id)
-            conversation = Conversation(tweet_id)
-            find_replays(collection, tweet_id, tweets_list, conversation)
-            conversation.set_depth()
-            conversation.set_width()
-            test_list.append(conversation.find_depth())
-            conversations_list.append(conversation)
-
+            tweet_text = root_tweet['text']
+            german = check_lang.check_german(tweet_text)
+            if german is True:
+                tweets_list.add(tweet_id)
+                conversation = Conversation(tweet_id)
+                find_replays(collection, tweet_id, tweets_list, conversation)
+                conversation.set_depth()
+                conversation.set_width()
+                test_list.add(conversation.find_depth())
+                conversations_list.append(conversation)
 
             if count % 10000 == 0:
                 print('in progress...' + '%.2f' % (count*100/float(number_of_root_tweets)) + '%')
                 # ############TEST###############
                 # break
+        # if count == 20000:
+        #     return conversations_list
 
     print 'There are: ' + str(len(tweets_list)) + 'tweets'
     print 'There are: ' + str(len(conversations_list)) + ' conversations'
     print 'Check: ' + str(sum(test_list))
+    print test_list
     return conversations_list
 
 
 def find_replays(collection, tweet, tweets_list, conversation):
-    replays = queries.search_replays(collection, tweet)
+    replays = mongoDBQueries.search_replays(collection, tweet)
     if replays is not None:
         for replay in replays:
-            tweet_id = replay['id']
-            tweet_id = int(tweet_id)
-            if tweet_id in tweets_list:
-                continue
-            else:
-                new_tweet = tweet_id
-                tweets_list.add(new_tweet)
+            tweet_text = replay['text']
+            if check_lang.check_german(tweet_text) is True:
+                tweet_id = replay['id']
+                tweet_id = int(tweet_id)
+                if tweet_id in tweets_list:
+                    continue
+                else:
+                    new_tweet = tweet_id
+                    tweets_list.add(new_tweet)
 
-                conversation.add_replay(new_tweet, tweet)
-                find_replays(collection, new_tweet, tweets_list, conversation)
+                    conversation.add_replay(new_tweet, tweet)
+                    find_replays(collection, new_tweet, tweets_list, conversation)
 
 
-def check_lang(collection_conversation, collection_raw_data):
-    # conversations_list = collection_conversation.find().batch_size(500)
-    #     queries.find_all(collection_conversation) ##maybe batch size if too slow
-    for conversation in collection_conversation.find().batch_size(500):
-        root_tweet_id = conversation['root_tweet_id']
-        tweets = collection_raw_data.find({'id': root_tweet_id})
-            # queries.find_by_id(collection_raw_data, root_tweet_id)
-        for tweet in tweets:
-            tweet_text = tweet['text']
-
-            print tweet_text
+# def check_lang(collection_conversation, collection_raw_data):
+#     # conversations_list = collection_conversation.find().batch_size(500)
+#     #     queries.find_all(collection_conversation) ##maybe batch size if too slow
+#     for conversation in collection_conversation.find().batch_size(500):
+#         root_tweet_id = conversation['root_tweet_id']
+#         tweets = collection_raw_data.find({'id': root_tweet_id})
+#             # queries.find_by_id(collection_raw_data, root_tweet_id)
+#         for tweet in tweets:
+#             tweet_text = tweet['text']
+#
+#             print tweet_text
 
 
 
 # collectionRawData = mongoDB_configuration.get_collection('DARecognition', 'rawTwitterData')
 # collectionConversationTree = mongoDB_configuration.get_collection('DARecognition', 'conversationTree')
-# check_lang(collectionConversationTree, collectionRawData)
