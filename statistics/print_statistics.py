4bd04ada5eeb80df989fc7b9ed728cc4adfefe94
Elina Zarisheva <elina.zarisheva@gmail.com>
2015-10-14 13:54:32 +0200

    statistics

--- statistics/print_statistics.py
+++ statistics/print_statistics.py
@@ -1,56 +1,31 @@
 __author__ = 'snownettle'
-import rawData_stat
-from twitter_objects import conversation
-from mongoDB import mongoDB_configuration
+from postgres import postgres_configuration
+from analysing_GS import analysis
+from prepare_gold_standard import rebuild_conversations
 
 
-def stats_for_raw_data():
-    print 'starting...'
-    collectionRawTwitterData = mongoDB_configuration.get_collection('DARecognition', 'rawTwitterData')
-    print 'Indexing ids ...'
-    collectionRawTwitterData.ensure_index('id')
-    print 'Indexing in_reply_to_status_id...'
-    collectionRawTwitterData.ensure_index('in_reply_to_status_id')
-    print 'Start building conversations'
-    conversation_list = conversation.build_conversation(collectionRawTwitterData)
-    rawData_stat.number_of_conversation_with_replays(conversation_list)
-    rawData_stat.depth_of_conversation(conversation_list)
-    rawData_stat.width_of_conversation(conversation_list)
-    rawData_stat.longest_conversation(conversation_list)
+def stats_for_gold_standard():
+    taxonomies = ['full', 'reduced', 'minimal']
+    connection, cursor = postgres_configuration.make_connection()
+    conversations_list = rebuild_conversations.build_conversation(cursor)
+    analysis.deep_distribution(conversations_list)
+    long_conversations_list, short_conversations_list = analysis.conversation_length_amount(conversations_list)
+    analysis.overall_segments(cursor)
+    analysis.segments_distribution(cursor)
+    analysis.feature_distribution(cursor)
+    for taxonomy in taxonomies:
+        print taxonomy + ' taxonomy'
+        analysis.da_unigrams(taxonomy, cursor)
+        analysis.length_distribution(taxonomy, cursor)
+        print 'bigrams'
+        analysis.da_bigrams(conversations_list, taxonomy, cursor)
+        print 'bigram in long conversations'
+        analysis.da_bigrams(long_conversations_list, taxonomy, cursor)
+        print 'bigrams in short conversations'
+        analysis.da_bigrams(short_conversations_list, taxonomy, cursor)
 
 
-# def stats_for_annotated_data():
-#     collectionAnnotatedData = mongoDB_configuration.get_collection('DARecognition', 'annotatedTwitterData')
-#     list_of_annotated_tweets = segmentation(collectionAnnotatedData)
-#     annotatedData_stat.number_of_annotated_tweet(list_of_annotated_tweets)
-#     annotatedData_stat.numbers_of_tweets_agreed_by_three(list_of_annotated_tweets)
-#
-#     print 'First merge of tags after reading the data' #choosing where is the number bigger
-#     #first merge of tags, after building the list
-#     list_of_annotated_tweets = merge_annotations(list_of_annotated_tweets)
-#     rewrite_segmentation(list_of_annotated_tweets)
-#     list_of_tweets_done, list_of_tweets_for_editing = annotatedData_stat.numbers_of_agreed_tweets_after_merging(list_of_annotated_tweets)
-#
-#     print 'First merge of tag children'
-#     #first merge of children
-#     da_taxonomy = build_da_taxonomy()
-#     merge_da_children(list_of_annotated_tweets, da_taxonomy)
-#     rewrite_segmentation(list_of_annotated_tweets)
-#     list_of_tweets_done, list_of_tweets_for_editing = annotatedData_stat.numbers_of_agreed_tweets_after_merging(list_of_annotated_tweets)
-#
-#     print 'Second merge of tags'
-#     #second merge of tags
-#     list_of_annotated_tweets = merge_annotations(list_of_annotated_tweets)
-#     rewrite_segmentation(list_of_annotated_tweets)
-#     list_of_tweets_done, list_of_tweets_for_editing = annotatedData_stat.numbers_of_agreed_tweets_after_merging(list_of_annotated_tweets)
-#
-#     annotatedData_stat.segments_in_tweet(list_of_tweets_done)
-#     write_to_xlsx_file(list_of_tweets_for_editing, '../DATA/tweet_to_edit.xlsx')
-#     write_to_xlsx_file(list_of_tweets_done, '../DATA/done_tweet.xlsx')
+stats_for_gold_standard()
 
-# print 'Stats for annotated data:'
-# stats_for_annotated_data()
-# print 'Stats for raw data: '
-# stats_for_raw_data()
 
 
